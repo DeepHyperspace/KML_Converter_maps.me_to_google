3303c3ef9e3c382aa738072c232c051770c95578
Frederik <frederikm1988@gmail.com>
2020-12-24 12:48:44 +0100

    Release 1.0

---
+++ KML_Converter/MainForm.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace GIS_Editor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.bt_load = new System.Windows.Forms.Button();
+            this.openKml = new System.Windows.Forms.OpenFileDialog();
+            this.bt_split = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lb_xmlstats = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lb_wait = new System.Windows.Forms.Label();
+            this.cb_country = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_load
+            // 
+            this.bt_load.Location = new System.Drawing.Point(15, 19);
+            this.bt_load.Name = "bt_load";
+            this.bt_load.Size = new System.Drawing.Size(128, 36);
+            this.bt_load.TabIndex = 1;
+            this.bt_load.Text = "open KML file";
+            this.bt_load.UseVisualStyleBackColor = true;
+            this.bt_load.Click += new System.EventHandler(this.bt_load_Click);
+            // 
+            // bt_split
+            // 
+            this.bt_split.Location = new System.Drawing.Point(21, 89);
+            this.bt_split.Name = "bt_split";
+            this.bt_split.Size = new System.Drawing.Size(128, 37);
+            this.bt_split.TabIndex = 2;
+            this.bt_split.Text = "convert and save KML";
+            this.bt_split.UseVisualStyleBackColor = true;
+            this.bt_split.Click += new System.EventHandler(this.bt_split_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(21, 37);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(128, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            // 
+            // lb_xmlstats
+            // 
+            this.lb_xmlstats.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lb_xmlstats.Location = new System.Drawing.Point(0, 0);
+            this.lb_xmlstats.Name = "lb_xmlstats";
+            this.lb_xmlstats.Size = new System.Drawing.Size(123, 54);
+            this.lb_xmlstats.TabIndex = 4;
+            this.lb_xmlstats.Text = "   ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.bt_load);
+            this.groupBox1.Location = new System.Drawing.Point(12, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 172);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1. Read maps.me KML";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lb_xmlstats);
+            this.panel1.Location = new System.Drawing.Point(19, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(123, 54);
+            this.panel1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lb_wait);
+            this.groupBox2.Controls.Add(this.cb_country);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.bt_split);
+            this.groupBox2.Location = new System.Drawing.Point(169, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 172);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2. Write Google KML";
+            // 
+            // lb_wait
+            // 
+            this.lb_wait.Location = new System.Drawing.Point(21, 132);
+            this.lb_wait.Name = "lb_wait";
+            this.lb_wait.Size = new System.Drawing.Size(146, 32);
+            this.lb_wait.TabIndex = 6;
+            this.lb_wait.Text = "-";
+            // 
+            // cb_country
+            // 
+            this.cb_country.FormattingEnabled = true;
+            this.cb_country.Items.AddRange(new object[] {
+            "Germany",
+            "France",
+            "Belgium",
+            "Luxembourg",
+            "Switzerland",
+            "Austria"});
+            this.cb_country.Location = new System.Drawing.Point(21, 63);
+            this.cb_country.Name = "cb_country";
+            this.cb_country.Size = new System.Drawing.Size(128, 21);
+            this.cb_country.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Placemark split count";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Check Color Hex";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save Template";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(348, 88);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(127, 172);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Testing";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 22);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Go To Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "blue";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KML_Converter.Properties.Resources.MapIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(69, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(87, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 60);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Welcome to the KML converter. You can use this program to split KML files. You ca" +
+    "n limit the number of placemarks and split them by country based on their geogra" +
+    "phic coordinates.";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(337, 18);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(138, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Created by Frederik Mussler";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 272);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KML-Converter maps.me to googlemaps";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bt_load;
+        private System.Windows.Forms.OpenFileDialog openKml;
+        private System.Windows.Forms.Button bt_split;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lb_xmlstats;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cb_country;
+        private System.Windows.Forms.Label lb_wait;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ KML_Converter/MainForm.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wibci.CountryReverseGeocode;
+using Wibci.CountryReverseGeocode.Models;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace GIS_Editor
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private CountryReverseGeocodeService gServce = new CountryReverseGeocodeService();
+
+
+        List<XmlNode> PlacemarkList = new List<XmlNode>();
+        List<XmlNode> OtherNodeList = new List<XmlNode>();
+        string ResultPath = Application.StartupPath + "/Result/";
+
+        XmlNode xmlDocNode;
+        private void bt_load_Click(object sender, EventArgs e)
+        {
+            openKml.InitialDirectory = Application.StartupPath;
+            openKml.Filter = "KML Files (*.kml)|*.kml";
+            openKml.FilterIndex = 1;
+            openKml.Multiselect = false;
+            openKml.RestoreDirectory = true;
+            openKml.ShowDialog();
+
+            string filename = openKml.FileName;
+            if (System.IO.File.Exists(filename))
+            {
+                xmlDocNode = LoadXML(filename);
+            }
+
+            if (xmlDocNode != null)
+            {
+                var msg = "Placemarks: " + Convert.ToString(PlacemarkList.Count) + Environment.NewLine +
+                "ChildNodes: " + Convert.ToString(xmlDocNode.ChildNodes.Count) + Environment.NewLine +
+                "OtherNodes: " + Convert.ToString(OtherNodeList.Count);
+
+                //MessageBox.Show(msg, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lb_xmlstats.Text = msg;
+            }
+
+        }
+
+        private void bt_split_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.GetFiles(ResultPath, "*.kml").Length == 0)
+            {
+                WriteXML(PlacemarkList, Convert.ToInt32(numericUpDown1.Value));
+            }
+            else
+            {
+                MessageBox.Show("Please delete all *.kml files in the 'Result' folder and try again.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //has matching *.wma files
+            }
+
+
+        }
+
+        public XmlNode LoadXML(string filename)
+        {
+            try
+            {
+                PlacemarkList.Clear();
+                OtherNodeList.Clear();
+                var xDocument = XDocument.Load(filename);
+                string sXML = xDocument.ToString();
+                System.Xml.XmlDocument xml = new System.Xml.XmlDocument();
+                xml.LoadXml(sXML);
+                XmlNode topnode = xml.DocumentElement;
+
+
+                if (topnode.Name == "kml" && topnode.FirstChild.Name == "Document")
+                {
+                    var docNode = topnode.FirstChild;
+
+                    foreach (XmlNode n in docNode.ChildNodes)
+                    {
+                        if (n.Name == "Placemark")
+                        {
+                            PlacemarkList.Add(n);
+                        }
+                        else
+                        {
+                            OtherNodeList.Add(n);
+                        }
+                    }
+                    MessageBox.Show("KML loaded successfully.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return docNode;
+                }
+                return null;
+            }
+            catch (InvalidCastException e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+
+            finally
+            {
+
+            }
+        }
+
+
+        private XmlDocument getNewTemplateDocument()
+        {
+            List<XmlNode> styleNodeList = new List<XmlNode>();
+            string filename = Application.StartupPath + "/Files/TemplateXML.xml";
+            XmlDocument xmlTemplate = new XmlDocument();
+            xmlTemplate.LoadXml(XDocument.Load(filename).ToString());
+
+            XmlDeclaration xmlDeclaration = xmlTemplate.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmlTemplate.DocumentElement;
+            xmlTemplate.InsertBefore(xmlDeclaration, root);
+            
+
+            if (xmlTemplate.DocumentElement.Name == "kml" && xmlTemplate.DocumentElement.FirstChild.Name == "Document")
+            {
+                XmlNodeList childs = xmlTemplate.DocumentElement.FirstChild.ChildNodes;
+                foreach (XmlNode n in childs)
+                {
+                    if (n.Name.Contains("Style"))
+                    {
+                        styleNodeList.Add(n);
+                    }
+                }
+            }
+            CopyStyleColorNodes(xmlTemplate, styleNodeList);
+
+            //Standardfarbe setzen
+            foreach (XmlNode n in styleNodeList)
+            {
+                SearchAndReplace_HEXCOLOR(n, "0288D1");
+            }
+
+            return xmlTemplate;
+        }
+
+
+        void CopyStyleColorNodes(XmlDocument xmlDocument, List<XmlNode> styleNodeList)
+        {
+            foreach (XmlNode n in OtherNodeList)
+            {
+                if (n.Name.Contains("Style"))
+                {
+                    string attr = n.Attributes.GetNamedItem("id").Value;
+                    if (attr.Contains("placemark-"))
+                    {
+                        string colorname = attr.Replace("placemark-", "");
+                        CopyStyleNodesToDocument(xmlDocument, styleNodeList, colorname);
+                    }
+                }
+            }
+        }
+
+        void PlacemarkConversion(XmlNode placemark)
+        {
+            foreach (XmlNode n in placemark.ChildNodes)
+            {
+                if (n.Name.Contains("styleUrl"))
+                {
+                    string styleUrl = n.InnerText;
+                    if (styleUrl.Contains("placemark-"))
+                    {
+                        string color = styleUrl.Replace("#placemark-", "");
+                        n.InnerText = "#icon-1899-" + GetHexColorAsString(color);
+                    }
+
+                }
+            }
+
+        }
+
+
+        private string GetHexColorAsString(string color)
+        {
+
+            switch (color)
+            {
+                case "deeppurple":
+                    return "580479";
+                case "deeporange":
+                    return "B27300";
+                case "bluegray":
+                    return "4E6E81";
+            }
+
+            int ColorValue = Color.FromName(color).ToArgb();
+            string ColorHex = string.Format("{0:x6}", ColorValue);
+            if (ColorHex.Length > 6)
+            {
+                int start = ColorHex.Length - 6;
+                ColorHex = ColorHex.Substring(start, 6);
+            }
+            return ColorHex;
+        }
+
+        void CopyStyleNodesToDocument(XmlDocument xmlDocument, List<XmlNode> styleNodeList, string color)
+        {
+            string ColorHex = GetHexColorAsString(color);
+
+            foreach (XmlNode styleNode in styleNodeList)
+            {
+                XmlNode newNode = xmlDocument.ImportNode(styleNode, true);
+                xmlDocument.DocumentElement.FirstChild.AppendChild(newNode);
+                SearchAndReplace_HEXCOLOR(newNode, ColorHex);
+            }
+
+        }
+
+        void SearchAndReplace_HEXCOLOR(XmlNode newNode, string ColorHex)
+        {
+            newNode.Attributes.GetNamedItem("id").Value = newNode.Attributes.GetNamedItem("id").Value.Replace("ReplaceWith_HEXCOLOR", ColorHex);
+            newNode.InnerXml = newNode.InnerXml.Replace("<color>ffd18802</color>", "<color>ff"+ ColorHex + "</color>");
+            newNode.InnerXml = newNode.InnerXml.Replace("ReplaceWith_HEXCOLOR", ColorHex);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var xmlDocument = getNewTemplateDocument();
+            xmlDocument.Save(Application.StartupPath + "/Result/TEST" + ".kml");
+        }
+
+        void WriteXML(List<XmlNode> PlacemarkList, int splitinc)
+        {
+            int i = 0;
+            int s = 0;
+            int x = 0;
+
+            var xmlDocument = getNewTemplateDocument();
+
+            System.IO.Directory.CreateDirectory(ResultPath);
+
+            foreach (XmlNode n in PlacemarkList)
+            {
+                x++;
+                if (cb_country.Text != "")
+                {
+                    if (IsInCountry(n, cb_country.Text))
+                    {
+                        XmlNode newNode = xmlDocument.ImportNode(n, true);
+                        PlacemarkConversion(newNode);
+                        xmlDocument.DocumentElement.FirstChild.AppendChild(newNode);
+                        i++;
+                    }
+                } else
+                {
+                    XmlNode newNode = xmlDocument.ImportNode(n, true);
+                    PlacemarkConversion(newNode);
+                    xmlDocument.DocumentElement.FirstChild.AppendChild(newNode);
+                    i++;
+                }
+
+                if (i == splitinc)
+                {
+                    i = 0;
+                    s++;
+                    xmlDocument.Save(ResultPath + cb_country.Text + "Split" + s + ".kml");
+                    xmlDocument = getNewTemplateDocument();
+                }
+
+                System.Windows.Forms.Application.DoEvents();
+                this.Enabled = false;
+                lb_wait.Text = "Items: " + Convert.ToString(x) + "/"  + Convert.ToString(i) + Environment.NewLine +  "Files: " + Convert.ToString(s);
+            }
+            s++;
+            xmlDocument.Save(ResultPath + cb_country.Text + "Split" + s + ".kml");
+            lb_wait.Text = "Items: " + Convert.ToString(x) + "/" + Convert.ToString(i) + Environment.NewLine + "Files: " + Convert.ToString(s);
+
+            System.Windows.Forms.Application.DoEvents();
+            this.Enabled = true;
+
+            MessageBox.Show("New KML files created successfull. Check the 'Result' folder.", "Hinweis", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            System.Diagnostics.Process.Start(ResultPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var color = textBox1.Text;
+            MessageBox.Show(GetHexColorAsString(color));
+        }
+
+
+        private bool IsInCountry(XmlNode placemark, string country)
+        {
+            foreach (XmlNode n in placemark.ChildNodes)
+            {
+                if (n.Name.Contains("Point") | n.Name.Contains("LineString"))
+                {
+                    if (n.ChildNodes.Item(0).Name == "coordinates")
+                    {
+                        char[] separators = new char[] { ',' };
+                        string[] longlat = n.ChildNodes.Item(0).InnerText.Split(separators);
+                        double lng = Convert.ToDouble(longlat[0], new CultureInfo("en-US"));
+                        double lat = Convert.ToDouble(longlat[1], new CultureInfo("en-US"));
+
+                        var location = new GeoLocation { Longitude = lng, Latitude = lat };
+                        var info = gServce.FindCountry(location);
+                        if (info == null) { return false; }
+                        return (info.Name == country);
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(ResultPath))
+            {
+                System.Diagnostics.Process.Start(ResultPath);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.frederikm.de/");
+        }
+    }
+}
+
